1cd9123e2ae9bfd0a9a18c1da587b0534263e111
ScorpiusG <scorpiusg59@gmail.com>
2024-10-12 10:53:55 +0800

    Add CanvasAspectRatioAutoMatch

---
+++ Scripts/CanvasAspectRatioAutoMatch.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasAspectRatioAutoMatch : MonoBehaviour
+{
+    private CanvasScaler canvasScaler;
+
+    private void Awake()
+    {
+        canvasScaler = GetComponent<CanvasScaler>();
+    }
+
+    private void Start()
+    {
+        Adapt();
+    }
+
+    public void Adapt()
+    {
+        if (canvasScaler == null)
+        {
+            canvasScaler = GetComponent<CanvasScaler>();
+        }
+        if (canvasScaler == null) return;
+
+        float screenRatioCurrent = (1f * Screen.width) / (1f * Screen.height);
+        float screenRatioPreferred = (1f * canvasScaler.referenceResolution.x) / (1f * canvasScaler.referenceResolution.y);
+
+        // Wider = Fixed height
+        if (screenRatioCurrent > screenRatioPreferred + 0.0001f)
+        {
+            canvasScaler.matchWidthOrHeight = 1f;
+        }
+        // Taller = Fixed width
+        else if (screenRatioCurrent < screenRatioPreferred - 0.0001f)
+        {
+            canvasScaler.matchWidthOrHeight = 0f;
+        }
+    }
+}
